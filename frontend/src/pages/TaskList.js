78429b9319ddb8d56a77388989118ab1846c33c8
Priyanshu Kumar <priyanshukumar9780@gmail.com>
2025-01-30 11:46:30 +0530

    Refactor Navbar component into its own file and update imports; enhance LoginPage with token check for redirection

--- frontend/src/pages/TaskList.js
+++ frontend/src/pages/TaskList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import EditTaskModal from './EditTaskModal'
+import EditTaskModal from '../components/EditTaskModal'
 import axios from "axios";
 
 const TaskList = () => {
